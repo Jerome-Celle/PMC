ae98189aa78006756f733baf40a0e8eae82d5c5d
Jerome Celle <jeffyer38130@gmail.com>
2015-11-02 10:46:39 +0100

    maj nom ID

--- EasterEggs/R2D2Sound/R2D2sound.js
+++ EasterEggs/R2D2Sound/R2D2sound.js
@@ -1,7 +1,7 @@
 function R2D2Sound(){
-	$('#PMC_searchresults').html('<audio preload="auto" id="son">' +
+	$('#PMC_searchresults').html('<audio preload="auto" id="EasterEggsR2D2Sound">' +
   									'<source src="beep.mp3" type="audio/mp3">' +
   									'<source src="beep.ogg" type="audio/ogg">' +
 								 '</audio>');
-	$('#id')[0].play();
+	$('#EasterEggsR2D2Sound').play();
 }

caa84a02d2acfd8ae582f45cf1777854a0331f0d
Nokte <sayhello@nokte.fr>
2015-11-07 02:28:54 +0100

    Modification Ordre + Apparence Slide Menu

--- navbar.php
+++ navbar.php
@@ -1,21 +1,30 @@
 <nav id="c-menu--slide-left" class="c-menu c-menu--slide-left">
-	<button class="c-menu__close"><img id="b_menu_close" src="img/dsn/PMC_icn_close.svg" style="width: 20px;"></button>
+	<img id="c_menu_info" src="img/dsn/SPGG_icn_info.svg" style="width: 25px;">
+	<button class="c-menu__close"><img src="img/dsn/PMC_icn_close.svg" style="width: 20px;"></button>
 	<ul class="c-menu__items">
+		<li  id="menuabout">
+			<h6>Design by <a href="http://www.nokte.fr" alt="Site internet Nokte.fr">Nokté</a></h6>
+			<h6>Code by ???</h6>
+		</li>
 		<li class="c-menu__item" id="menuhome">
 			<a href="index.php" class="c-menu__link">Accueil</a>
 		</li>
+		<li class="c-menu__item" id="menuDC">
+			<a href="dccomics.php" class="c-menu__link">Univers DC Comics</a>
+		</li>
+		<li class="c-menu__item" id="menudisney">
+			<a href="disney.php" class="c-menu__link">Univers Disney</a>
+		</li>
 		<li class="c-menu__item" id="menumarvel">
-			<a href="marvel.php" class="c-menu__link">Portail Marvel</a>
+			<a href="marvel.php" class="c-menu__link">Univers Marvel</a>
 		</li>
 		<li class="c-menu__item" id="menusw">
-			<a href="starwars.php" class="c-menu__link">Portail Star Wars</a>
+			<a href="starwars.php" class="c-menu__link">Univers Star Wars</a>
 		</li>
-		<li class="c-menu__item" id="menuDC">
-			<a href="dccomics.php" class="c-menu__link">Portail DC Comics</a>
-		</li>
-		<li class="c-menu__item">
+		<li class="c-menu__item" id="menupropos">
 			<a href="contact.php" class="c-menu__link">Proposer un film</a>
 		</li>
+		
 	</ul>
 </nav>
 
